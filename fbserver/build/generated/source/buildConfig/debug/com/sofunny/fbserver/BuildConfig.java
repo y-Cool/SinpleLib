6d37b9eaec97f455c887e72c07f6d5fee3d57ca8
y-Cool <774908055@qq.com>
2021-12-31 18:49:53 +0800

    Add files via upload

---
+++ fbserver/build/generated/source/buildConfig/debug/com/sofunny/fbserver/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sofunny.fbserver;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.sofunny.fbserver";
+  public static final String BUILD_TYPE = "debug";
+}
